2b3c90a6155371481b992d765a0f6bf38bef98c5
MatheusBlanco <msallesblanco@gmail.com>
2024-07-10 20:00:12 -0300

    fix: :bug: Fixes skeleton

--- src/screens/ExtraPage/index.tsx
+++ src/screens/ExtraPage/index.tsx
@@ -6,21 +6,39 @@ import { Title } from '@/components/StyledComponents/Title';
 import { getBrazilNews } from '@/services/news';
 import { globalStyleSheet } from '@/styles/globalStyleSheet';
 import { useQuery } from '@tanstack/react-query';
+import { MotiView } from 'moti';
 import { Skeleton } from 'moti/skeleton';
-import { FlatList, View } from 'react-native';
+import React from 'react';
+import { FlatList, StyleSheet, useColorScheme, View } from 'react-native';
+
+const Spacer = ({ height = 16 }) => <MotiView style={{ height }} />;
 
 export const ExtraPage = () => {
   const { data, isLoading } = useQuery({
     queryKey: ['brazil-news'],
     queryFn: getBrazilNews,
   });
+  const colorScheme = useColorScheme();
 
   return (
     <Container>
       <StyledMainContainer style={globalStyleSheet.flex}>
         <SimpleFlexColumn style={{ ...globalStyleSheet.flex, ...globalStyleSheet.gap12 }}>
           <Title>{"Brazil's headlines: "}</Title>
-          <Skeleton show={isLoading}>
+          {isLoading ? (
+            <MotiView
+              transition={{
+                type: 'timing',
+              }}
+              animate={{ backgroundColor: 'transparent' }}
+            >
+              <Skeleton colorMode={colorScheme as 'light' | 'dark'} width={'100%'} />
+              <Spacer height={8} />
+              <Skeleton colorMode={colorScheme as 'light' | 'dark'} width={'100%'} />
+              <Spacer height={8} />
+              <Skeleton colorMode={colorScheme as 'light' | 'dark'} width={'50%'} />
+            </MotiView>
+          ) : (
             <FlatList
               data={data?.articles}
               renderItem={({ item }) => {
@@ -32,9 +50,19 @@ export const ExtraPage = () => {
                 );
               }}
             />
-          </Skeleton>
+          )}
         </SimpleFlexColumn>
       </StyledMainContainer>
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  padded: {
+    padding: 16,
+  },
+});
